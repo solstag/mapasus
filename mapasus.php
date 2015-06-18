a9d7e43b07c9bafdca436b9da9969fc43d600b2a
Oda <oda@member.fsf.org>
2015-06-18 14:53:40 -0400

    split into functions and switch to google geocoding

--- mapasus.php
+++ mapasus.php
@@ -25,6 +25,7 @@ function _mapasus_getdata($text,$field){
   $val=substr($text,$idx1+$len,$idx2-$idx1-$len-1);
   return $val;
 }
+
 function mapasus_init($a){
   // deixei os if's separados para facilitar leitura
   if(! local_channel())
@@ -33,14 +34,18 @@ function mapasus_init($a){
     return;
   if(argc()==0)
     return;
+  if(argv(1)==="geocode"){
+    mapasus_cron_geocode();
+    killme();
+  }
   if(argv(1)==="data"){
     $r = q("SELECT body, id, uid
             FROM item
-            WHERE author_xchan = '%s' AND
+            WHERE owner_xchan = '%s' AND
             body like '%s';
             ",
             dbesc("WetA6eJ6XSHEtDUEcbWpXa-7m3ypnNtx1Dec7C6oC-0VVKc29WJ2UUbXHcBd-tdA6HKEk-Zn5sXUDvoj5DHGiA"),
-            dbesc("%[b]Endereço:[/b]%")
+            dbesc("%[b]Coordenadas:[/b]%")
           );
     // AND body LIKE '%[b]EndereÃ§o:[/b]%'
     $ret=array();
@@ -49,56 +54,17 @@ function mapasus_init($a){
       $item['body'] = $row['body'];
       $item['uid'] = $row['uid'];
       $item['id'] = $row['id'];
-      $idx = strpos($item['body'],"[b]Coordenadas:[/b]");
-
-      if($idx===false){
-        //geocode
-        logger('mapasus geocoding: '.$item['id'].'-'.$item['uid'], LOGGER_DEBUG);
-        $addr1 = _mapasus_getdata($item['body'],"[b]Endereço:[/b]");
-        $addr2 = _mapasus_getdata($item['body'],"[b]Complemento:[/b]");
-        $zip = _mapasus_getdata($item['body'],"[b]CEP:[/b]");
-        $addr=array(
-          'street'=>$addr1,
-          'state'=>'Sao Paulo',
-          'country'=>'BR',
-          'format'=>'json',
-          'limit'=>1
-        );
-        $jsonurl = "http://nominatim.openstreetmap.org/search?".http_build_query($addr);
-        $geocode = json_decode(file_get_contents($jsonurl));
-        if(count($geocode)>0){
-          logger('mapasus geocoding: success', LOGGER_DEBUG);
-          $coords=$geocode[0]->lat.",".$geocode[0]->lon;
-          $idx2 = strpos($item['body'],"[b]CEP:[/b]"); // insere as coords depois do cep
-          $idx2 = strpos($item['body'],"\n",$idx2); // primeira quebra de linha apor o cep
-          $item['body'] = substr_replace($item['body'], "[b]Coordenadas:[/b]".$coords."\r\n", $idx2+1, 0);
-          item_store_update($item);
-          $item['lat']=$geocode[0]->lat;
-          $item['lon']=$geocode[0]->lon;
-          $item['body']=bbcode($item['body']);
-          $ret[]=$item;
-        }else{
-          logger('mapasus geocoding: fail', LOGGER_DEBUG);
-          $coords="Falha na geocodificação. Corrija o endereço e remova esta linha.";
-          $idx2 = strpos($item['body'],"[b]CEP:[/b]"); // insere as coords depois do cep
-          $idx2 = strpos($item['body'],"\n",$idx2); // primeira quebra de linha apor o cep
-          $item['body'] = substr_replace($item['body'], "[b]Coordenadas:[/b]".$coords."\r\n", $idx2+1, 0);
-          item_store_update($item);
-        }
+      $coords = _mapasus_getdata($item['body'],"[b]Coordenadas:[/b]");
+      if($coords!="Falha na geocodificação. Corrija o endereço e remova esta linha."){
+        $aux=explode(",",$coords);
+        $item['lat']=$aux[0];
+        $item['lon']=$aux[1];
+        $item['body']=bbcode($item['body']);
+        $ret[]=$item;
       }else{
-        $coords = _mapasus_getdata($item['body'],"[b]Coordenadas:[/b]");
-        if($coords!="Falha na geocodificação. Corrija o endereço e remova esta linha."){
-          $aux=explode(",",$coords);
-          $item['lat']=$aux[0];
-          $item['lon']=$aux[1];
-          $item['body']=bbcode($item['body']);
-          $ret[]=$item;
-        }else{
-          logger('mapasus geocoding: failed', LOGGER_DEBUG);
-        }
+        logger('mapasus geocoding: failed', LOGGER_DEBUG);
       }
     }
-//     print_r($ret);
     echo json_encode($ret);
     killme();
   }
@@ -135,4 +101,59 @@ function mapasus_content($a){
   ));
 
   return $o;
+}
+
+function mapasus_cron_geocode(){
+  $delay=200;
+  $max=100;
+  $r = q("SELECT body, id, uid
+          FROM item
+          WHERE owner_xchan = '%s' AND
+          body like '%s' AND
+          body NOT like '%s'
+          LIMIT %d;
+          ",
+          dbesc("WetA6eJ6XSHEtDUEcbWpXa-7m3ypnNtx1Dec7C6oC-0VVKc29WJ2UUbXHcBd-tdA6HKEk-Zn5sXUDvoj5DHGiA"),
+          dbesc("%[b]Endereço:[/b]%"),
+          dbesc("%[b]Coordenadas:[/b]%"),
+          intval($max)
+        );
+  foreach($r as $i=>$row){
+    $item=array();
+    $item['body'] = $row['body'];
+    $item['uid'] = $row['uid'];
+    $item['id'] = $row['id'];
+
+    logger('mapasus geocoding: '.$item['id'].'-'.$item['uid'], LOGGER_DEBUG);
+
+//     $zip = _mapasus_getdata($item['body'],"[b]CEP:[/b]");
+    $addr=array(
+      'address'=>_mapasus_getdata($item['body'],"[b]Endereço:[/b]"),
+      'components'=>'country:BR|administrative_area:SP|locality:São Paulo',
+      'key'=>''
+    );
+    $jsonurl = "https://maps.googleapis.com/maps/api/geocode/json?".http_build_query($addr);
+    $geocode = json_decode(file_get_contents($jsonurl));
+    if($geocode->status == "OK" && count($geocode->results)>0 && $geocode->results[0]->types[0]!='locality'){
+      logger('mapasus geocoding: success', LOGGER_DEBUG);
+      $lat=$geocode->results[0]->geometry->location->lat;
+      $lon=$geocode->results[0]->geometry->location->lng;
+      $coords=$lat.",".$lon;
+      $idx2 = strpos($item['body'],"[b]CEP:[/b]"); // insere as coords depois do cep
+      $idx2 = strpos($item['body'],"\n",$idx2); // primeira quebra de linha apos o cep
+      $item['body'] = substr_replace($item['body'], "[b]Coordenadas:[/b]".$coords."\r\n", $idx2+1, 0);
+      item_store_update($item);
+    } else if($geocode->status == "OVER_QUERY_LIMIT") {
+      // too fast
+      $delay += 200;
+    }else{
+      logger('mapasus geocoding: fail', LOGGER_DEBUG);
+      $coords="Falha na geocodificação. Corrija o endereço e remova esta linha.";
+      $idx2 = strpos($item['body'],"[b]CEP:[/b]"); // insere as coords depois do cep
+      $idx2 = strpos($item['body'],"\n",$idx2); // primeira quebra de linha apor o cep
+      $item['body'] = substr_replace($item['body'], "[b]Coordenadas:[/b]".$coords."\r\n", $idx2+1, 0);
+      item_store_update($item);
+    }
+    usleep($delay);
+  }
 }
